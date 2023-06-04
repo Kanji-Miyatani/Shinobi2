8d10d15c329bc865d5458e6f6397d715263065a1
Kanji Miyatani <kanji09012458@gmail.com>
2023-06-04 18:55:41 +0900

    Userのリポジトリ作成

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Shinobi2
 伝説のサーバー「Shinobi」の閉鎖から半年。
 後継となる新たなチャットツールの作成に励んでおります。
+
+#参考URL
+・ [【React×Express】フルスタックでSPAの認証機能を実装してみた。](https://zenn.dev/masatotezuka/articles/9d9f9f9812f239)
+・[Prisma公式Doc](https://www.prisma.io/docs/concepts)
+・[mysqlのDockerのイメージ作成失敗解決方法](https://syoukinkubi.com/?p=941)
--- backend/src/repository/UsersRepository.ts
+++ backend/src/repository/UsersRepository.ts
@@ -1,3 +1,19 @@
-import mysql from 'mysql'
+import {User,PrismaClient} from '@prisma/client'
+const prisma = new PrismaClient();
+export const selectAll=async () : Promise<User[]>=>{
+    const allUsers = await prisma.user.findMany({
+    })
+    return allUsers;
+}
 
-fetch
+export const selectOne=async (id : Number) : Promise<User[]>=>{
+    const allUsers = await prisma.user.findMany({
+    })
+    return allUsers;
+}
+
+export  const create=async (user:User)=>{
+    await prisma.user.create({
+        data: user,
+      })
+}
