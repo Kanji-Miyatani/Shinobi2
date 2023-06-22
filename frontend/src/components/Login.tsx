c4b56b19f25b7d6cc910f2617b1c4ed5767b8e74
Kanji Miyatani <kanji09012458@gmail.com>
2023-06-22 11:47:43 +0900

    ログイン時の遷移先修正

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -58,7 +58,7 @@ function Login() {
         } 
         loginApi(req).then((body)=>{
             if(body.result){
-                navigate("/Rooms");
+                navigate("/");
             } else{
                 addError("password",body.message);
             }
--- frontend/src/services/socketService.ts
+++ frontend/src/services/socketService.ts
@@ -24,7 +24,6 @@ export const useSocket = ()=>{
 
     useEffect(()=>{
         const onConnection = ()=>{
-            
         }
     },[]);
 
