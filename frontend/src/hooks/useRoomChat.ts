2e93348786f0dac135acb0252da5c9c23d75d7c7
Kanji Miyatani <kanji09012458@gmail.com>
2023-06-30 19:35:12 +0900

    フロントリファクタリング

--- frontend/src/hooks/useRoomChat.ts
+++ frontend/src/hooks/useRoomChat.ts
@@ -1,24 +1,24 @@
 import { useEffect, useState } from 'react';
 import {io} from 'socket.io-client';
 import {useCookies} from 'react-cookie';
-import { Room,User, getOne} from '../api/rooms';
+import * as roomApi from '../api/rooms';
 import { Chat } from '../api/chat';
 import { Socket } from 'dgram';
 import { getURL } from '../api/URL';
 
 const URL =getURL( process.env.NODE_ENV === 'production' ? "/" : '/');
-const socket =io(URL, {
+let socket =io(URL, {
       withCredentials:true,
-    transports: [ 'websocket' ] 
+      transports: [ 'websocket' ] 
   })
 export const useRoomChat = (roomId:string)=>{
     const [isJoining,setIsJoining] = useState(false);
     const [messages,setMessages] =useState<Chat[]>(new Array<Chat>());
-    const [roomInfo,setRoomInfo] = useState<Room>();
+    const [roomInfo,setRoomInfo] = useState<roomApi.Room>();
     //クッキーからjwtトークンの取得
     const [cookies, setCookie, removeCookie]  =useCookies(["jwtToken"]);
     const token = cookies["jwtToken"];
-    const getUser = (id : number):User=>{
+    const getUser = (id : number):roomApi.User=>{
         const user = roomInfo?.users.find(x=>x.id===id);
         if(!user)
         {
@@ -27,16 +27,15 @@ export const useRoomChat = (roomId:string)=>{
         return user;
     }
     //入室処理
-    const joinRoom =(roomId:string):Promise<Room>=>{
-        return new Promise<Room>((resolve,reject)=>{
+    const joinRoom =(roomId:string):Promise<roomApi.Room>=>{
+        return new Promise<roomApi.Room>((resolve,reject)=>{
             //部屋情報取得APIを叩いた後にsocketで入室情報をブロードキャスト
-            getOne(roomId).then((data)=>{
-                socket.emit('join room',{roomId:roomId},()=>{
-                    setIsJoining(true);
+            socket.emit('join room',{roomId:roomId},()=>{
+                roomApi.getOne(roomId).then((data)=>{
+                    return resolve(data);
+                }).catch((e)=>{
+                    return reject(e);
                 });
-                return resolve(data);
-            }).catch((e)=>{
-                return reject(e);
             });
         })
     };
@@ -45,16 +44,18 @@ export const useRoomChat = (roomId:string)=>{
         socket.emit('message',msg);
     }
     useEffect(()=>{
+        socket=io(URL, {
+            withCredentials:true,
+            transports: [ 'websocket' ] 
+        })
         //入室処理
         joinRoom(roomId).then((data)=>{
-            console.log('入室データ');
             setMessages(data.chats);
             setRoomInfo(roomInfo=>data);
+            console.log('入室メンバー');
             console.log(data.users);
-            console.log(roomInfo);
-            
             //他ユーザの入室時K処理
-            socket.on("join room",(users:User[])=>{
+            socket.on("join room",(users:roomApi.User[])=>{
                 
             });
             //メッセージ受信処理
@@ -63,16 +64,18 @@ export const useRoomChat = (roomId:string)=>{
                 console.log(message);
                 setMessages((messages)=>[...messages,message]);
             });
+            //入室完了
+            setTimeout(()=>{
+                setIsJoining(true);
+            },200);
         }).catch(()=>{
             console.log('CANNNOT ENTRY ROOM')
-            //入室時例外処理はどうすればいいかわからないので未定
-            //多分エラーページに飛ばす。エラー内容は不明でいい。
+            //入室時例外処理はどうすればいいかわからないので未定。
         });
         return () => {
             console.log("disconnected!")
-            socket.on("disconnect",()=>{
-            });
-        };
+            socket.disconnect();
+        }
     },[]);
 
     return {
