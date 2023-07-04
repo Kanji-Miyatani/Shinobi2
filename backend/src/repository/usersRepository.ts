0d02c8901ed6be1fc6849643f8c0e9b5c6e73828
Kanji Miyatani <kanji09012458@gmail.com>
2023-07-04 16:45:27 +0900

    userRepository.ts追加

---
+++ backend/src/repository/usersRepository.ts
@@ -0,0 +1,78 @@
+import  {User,PrismaClient} from '@prisma/client'
+const prisma = new PrismaClient();
+
+
+export const selectAll=async () : Promise<User[]>=>{
+    const allUsers = await prisma.user.findMany({
+    })
+    return allUsers;
+}
+//任意の部屋に入室中のユーザーをすべて取得
+export const selectInRoom=async (roomId:string) : Promise<User[]>=>{
+    const allUsers = await prisma.user.findMany({
+        where:{
+            roomId : roomId,
+        }
+    })
+    return allUsers;
+}
+
+export const selectOne=async (email : string | undefined) : Promise<User | null>=>{
+    const user = await prisma.user.findFirst({
+        where:{
+            mailaddress : email,
+        }
+    })
+    return user;
+}
+export const selectOneByID=async (id : number ) : Promise<User | null>=>{
+    const user = await prisma.user.findFirst({
+        where:{
+            id : id,
+        }
+    })
+    return user;
+}
+export const selectIdFromSocketId=async (socketId:string) : Promise<number>=>{
+    const user = await prisma.user.findFirst({
+        where:{
+            socketId : socketId, 
+        },
+        select:{
+            id:true
+        }
+    })
+    if(user===null){
+       return 0;
+    }
+    return user.id;
+}
+
+export const create=async (
+    name:string,
+    email:string,
+    pass:string,
+    )=>{
+    await prisma.user.create({
+        data: {
+            name:name,
+            password:pass,
+            created_at:new Date(),
+            mailaddress:email,
+            characterId:"shinobi"//デフォルトキャラ
+        }
+        
+    })
+}
+// 部屋更新
+export const updateRoom=async (userId:number,roomId:string |null,socketId:string|null)=>{
+    await prisma.user.update({
+        where:{
+            id : userId
+        },
+        data:{
+            roomId : roomId,
+            socketId :socketId
+        }
+      });
+}
