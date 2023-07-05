8d5e52a450deca793661cd15cbc9385ba5c10331
Kanji Miyatani <kanji09012458@gmail.com>
2023-07-05 13:39:34 +0900

    環境変数を環境ごとに分ける

--- frontend/src/api/URL.ts
+++ frontend/src/api/URL.ts
@@ -1,5 +1,5 @@
 export const getURL =(path:string):string=>{
-    console.log(process.env.REACT_APP_APIURL)
+    console.log(process.env.REACT_APP_APIURL);
     const baseURL = process.env.REACT_APP_APIURL ?? "https://localhost:3001";
     if(baseURL==='/')return path;
     return `${baseURL}${path}`;
