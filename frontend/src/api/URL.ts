ecae063cef77cc54c7f151d806794acb2685060b
Kanji Miyatani <kanji09012458@gmail.com>
2023-07-05 13:10:20 +0900

    getURL.ts変更

--- frontend/src/api/URL.ts
+++ frontend/src/api/URL.ts
@@ -1,6 +1,6 @@
 export const getURL =(path:string):string=>{
-    console.log(process.env.APIURL)
-    const baseURL = process.env.APIURL ?? "https://localhost:3001";
+    console.log(process.env.REACT_APP_APIURL)
+    const baseURL = process.env.REACT_APP_APIURL ?? "https://localhost:3001";
     if(baseURL==='/')return path;
     return `${baseURL}${path}`;
 }
